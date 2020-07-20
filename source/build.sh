507100a18b0dd4b81b5b2fab709bbae22d188694
Sachit-Agarwal1 <sachit.agarwal1@gmail.com>
2020-07-19 20:45:32 -0700

    Updated .gitignore

--- source/build.sh
+++ source/build.sh
@@ -2,16 +2,16 @@
 
 CARLSIM4_INSTALL_DIR=$(HOME)/CARL
 
-swig -c++ -python carlsim.i
-mkdir -p bin/
-mv carlsim.py bin/
+#swig -c++ -python carlsim.i
+#mkdir -p bin/
+#mv carlsim.py bin/
 
 # TO BUILD FOR PYTHON2
-python2 setup.py build_ext -b ./bin/ -t ./bin/wrap --warnings --define __NO_CUDA__ --include <HOME>/CARL/include:/usr/local/cuda/include:/usr/local/cuda/samples/common/inc
+#python2 setup.py build_ext -b ./bin/ -t ./bin/wrap --warnings --define __NO_CUDA__ --include <HOME>/CARL/include:/usr/local/cuda/include:/usr/local/cuda/samples/common/inc
 
 # TO BUILD FOR PYTHON3
-# swig -c++ -python -py3 carlsim.i
-# python3 setup.py build_ext --inplace --define __NO_CUDA__ --include ../carlsim/kernel/inc/:../carlsim/monitor/:../carlsim/interface/inc/:../tools/spike_generators/
+ swig -c++ -python -py3 carlsim.i
+ python3 setup.py build_ext --inplace --define __NO_CUDA__ --include ../../CARLsim4/carlsim/kernel/inc/:../../CARLsim4/carlsim/monitor/:../../CARLsim4/carlsim/interface/inc/:../../CARLsim4/tools/spike_generators/
 
 # TO BUILD FOR CUDA SUPPORT
 # TODO: Figure out what needs including to fix errors
